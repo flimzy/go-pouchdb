a6ac73f5c6b4a348461fceb5223b8c42731f4a94
Jonathan Hall <flimzy@flimzy.com>
2016-09-03 16:57:30 +0200

    Move options to their own file

---
+++ doc.go
@@ -0,0 +1,11 @@
+// Package pouchdb provides GopherJS bindings for PouchDB.
+// Whenever possible, the PouchDB function calls have been made more
+// Go idiomatic. This means:
+//  - They don't take optional arguments. Where appropriate, multiple
+//    versions of a function exist with different argument lists.
+//  - They don't use call backs or return Promises
+//  - They have been made synchronous. If you need asynchronous operation,
+//    wrap your calls in goroutines
+//  - They return errors as the last return value (Go style) rather than the
+//    first (JS style)
+package pouchdb
