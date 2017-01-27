ad695dc995bf4837cbf4d59188f9efef96810c51
Jonathan Hall <flimzy@flimzy.com>
2017-01-27 21:27:32 +0100

    Add benchmark for ConvertJSObject

--- pouchdb_test.go
+++ pouchdb_test.go
@@ -300,3 +300,21 @@ func TestEvents(t *testing.T) {
 	}
 	close(eventsCh)
 }
+
+func BenchmarkConvertJSObject(b *testing.B) {
+	jsObj := js.Global.Get("Object").New()
+	jsObj.Set("foo", "bar")
+	jsObj.Set("bar", 100)
+	jsObj.Set("baz", true)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		x := struct {
+			Foo string `json:"foo"`
+			Bar int    `json:"bar"`
+			Baz bool   `json:"baz"`
+		}{}
+		if err := ConvertJSObject(jsObj, &x); err != nil {
+			panic(err)
+		}
+	}
+}
